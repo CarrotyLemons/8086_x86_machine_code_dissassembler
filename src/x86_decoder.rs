1621445882d346cb7cd25b0b5eabf003baea614d
CarrotyLemons <drdavidlovett@gmail.com>
2026-02-18 00:11:20 +0100

    BUGFIX: Moved repeating 'bits 16'

--- src/x86_decoder.rs
+++ src/x86_decoder.rs
@@ -6,6 +6,8 @@ use std::io::Write;
 pub fn decode_instructions(source_file: Vec<u8>, mut destination_file: File) {
     let mut source_file = source_file.into_iter();
 
+    writeln!(destination_file, "bits 16").unwrap();
+
     loop {
         let read_instruction = match extract_instruction(&mut source_file) {
             Ok(value) => value,
@@ -17,7 +19,6 @@ pub fn decode_instructions(source_file: Vec<u8>, mut destination_file: File) {
         match read_instruction {
             Some(instruction) => match instruction {
                 Instructions::Move(move_instruction) => {
-                    writeln!(destination_file, "bits 16").unwrap();
                     writeln!(
                         destination_file,
                         "mov {}, {}",
